8020650cc3235c496044a0f877862107ab12e4a0
armin-164 <arminsadr164@hotmail.com>
2024-01-31 19:55:24 +0100

    Enable CORS and add MongoDB connection

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+let cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +14,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://127.0.0.1:27017')
+.then(client => {
+    console.log('Connection to database is succesful');
+
+    const db = client.db('fornamn-efternam');
+    app.locals.db = db;
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
