3ada681b9b82781b5a77f1d3a06bc2948e1df31a
armin-164 <arminsadr164@hotmail.com>
2024-01-31 22:37:22 +0100

    Rewrite function to be arrow function, fetch the users db collection and respond with JSON of the users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res) => {
+  req.app.locals.db.collection("users").find().toArray()
+  .then(users => res.json(users))
 });
 
 module.exports = router;
