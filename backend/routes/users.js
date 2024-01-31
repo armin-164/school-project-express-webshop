72002b98b4fe60ad0b976c8cf2a83427a29ae2fc
armin-164 <arminsadr164@hotmail.com>
2024-01-31 23:39:47 +0100

    feat/improvement: Implement POST route. Add error handling for GET route.
    Add error handling for GET route at /api/users endpoint.
    Implement POST route to add data to db collection (users) and
    add response/error handling.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,6 +5,23 @@ var router = express.Router();
 router.get('/', (req, res) => {
   req.app.locals.db.collection("users").find().toArray()
   .then(users => res.json(users))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({err: "Internal Service Error"})
+  })
 });
+
+router.post('/add', (req, res) => {
+  req.app.locals.db.collection("users").insertOne(req.body)
+    .then(result => {
+      console.log(result);
+      res.status(201).json({ message: "User added successfully" });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
+});
+
 
 module.exports = router;
