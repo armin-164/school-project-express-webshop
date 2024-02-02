5e1800157e870e4a487e981da8f20e42cdba7a52
armin-164 <arminsadr164@hotmail.com>
2024-02-02 16:08:53 +0100

    feat: add GET method to retrieve all products

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,4 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET all products */
+router.get('/', (req, res) => {
+    req.app.locals.db.collection('products').find().toArray()
+    .then(products => {
+
+        // Check if collection is empty
+        if (products.length >= 1) {
+            res.json(products);
+        }
+        else {
+            res.status(404).json({message: 'Products not found'});
+        }
+        
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "Internal Server Error" });
+    })
+})
+
+
 module.exports = router;
