00d8c95b560438ee569927fa104734890d144718
armin-164 <arminsadr164@hotmail.com>
2024-02-02 18:23:51 +0100

    feat: Implement POST method to add category

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,4 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+/* POST method to create category. Key is needed */
+router.post('/add', (req, res) => {
+    let category = req.body;
+    
+    if (!category.hasOwnProperty('token')) {
+        res.status(401).json( {message: "Key/token is missing"} );
+    }
+
+    else {
+        req.app.locals.db.collection('categories').insertOne(category)
+        .then(result => {
+            console.log(result);
+            res.status(201).json( {message: "Category added successfully"} )
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "Internal Server Error" });
+        })
+    }
+})
+
 module.exports = router;
