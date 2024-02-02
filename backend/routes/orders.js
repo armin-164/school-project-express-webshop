ad6b2a230c8e7b47c631c2f8e088bb078d0909ef
armin-164 <arminsadr164@hotmail.com>
2024-02-02 17:03:17 +0100

    feat: add POST method to create order

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,4 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+router.post('/add', (req, res) => {
+    req.app.locals.db.collection('orders').insertOne(req.body)
+    .then(result => {
+        console.log(result);
+        res.json({message: 'Product added successfully'});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "Internal Server Error" });
+    })
+})
+
 module.exports = router;
