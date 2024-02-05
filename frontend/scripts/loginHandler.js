384ebd97f9b3e135c5462dad48448bfac6773278
armin-164 <arminsadr164@hotmail.com>
2024-02-05 21:08:02 +0100

    feat: Add dynamic login popup
    Introduce logic to display a popup div on element click
    Check localStorage for user presence
    Append login form to the popup if user is not found

---
+++ frontend/scripts/loginHandler.js
@@ -0,0 +1,57 @@
+function loginUser() {
+  const popupDiv = document.querySelector('.login-popup');
+  popupDiv.innerHTML = '';
+
+  const formContainer = document.createElement('form');
+  formContainer.classList.add('form-container');
+
+  const closeNavButton = document.createElement('span');
+  closeNavButton.classList.add('material-symbols-outlined');
+  closeNavButton.innerText = 'close';
+
+  const loginTitle = document.createElement('h2');
+  loginTitle.innerText = 'Log in';
+
+  const inputEmail = document.createElement('input');
+  inputEmail.placeholder = 'Email';
+  inputEmail.type = 'email';
+
+  const inputPassword = document.createElement('input');
+  inputPassword.placeholder = 'Password';
+
+  const loginBtn = document.createElement('button');
+  loginBtn.innerText = 'Login';
+
+  const createUserBtn = document.createElement('button');
+  createUserBtn.innerText = 'Create an account';
+
+  formContainer.append(
+    closeNavButton,
+    loginTitle,
+    inputEmail,
+    inputPassword,
+    loginBtn,
+    createUserBtn,
+  );
+
+  return formContainer;
+}
+
+function initializeLoginHandler() {
+  const userProfile = document.getElementById('user-profile');
+  const user = localStorage.getItem('user');
+
+  const popupDiv = document.createElement('div');
+  popupDiv.classList.add('login-popup', 'hidden');
+  document.body.appendChild(popupDiv);
+
+  if (!user) {
+    popupDiv.appendChild(loginUser());
+  }
+
+  userProfile.addEventListener('click', () => {
+    popupDiv.classList.toggle('active');
+  });
+}
+
+export default initializeLoginHandler;
