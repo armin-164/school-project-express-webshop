5e6c82df12b17ee712ce5c6c67027f2b2b71961c
armin-164 <arminsadr164@hotmail.com>
2024-02-05 18:26:31 +0100

    feat: add function that uses Fetch API to retrieve products

--- frontend/scripts/homepage.js
+++ frontend/scripts/homepage.js
@@ -29,3 +29,11 @@ function createHomepageElements(array) {
 
   mainDiv.appendChild(productsContainer);
 }
+
+function displayHomepage() {
+  fetch('http://localhost:3000/api/products')
+    .then((res) => res.json())
+    .then((data) => createHomepageElements(data));
+}
+
+export default displayHomepage;
