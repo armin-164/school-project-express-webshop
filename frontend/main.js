37db4dbd2d1acd332bab9385836cfdb96707bc53
armin-164 <arminsadr164@hotmail.com>
2024-02-05 22:40:48 +0100

    feat: import and execute intializeLoginHandler

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,6 @@
 import './style.scss';
 import displayHomepage from './scripts/homepage';
+import initializeLoginHandler from './scripts/loginHandler';
 
+initializeLoginHandler();
 displayHomepage();
