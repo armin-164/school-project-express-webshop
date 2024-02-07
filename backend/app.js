821dfa8f46bcb0a0cbe1b9be6bee2d1bd15138fe
armin-164 <arminsadr164@hotmail.com>
2024-02-07 22:27:05 +0100

    refactor: update mongoDB connection to use env variable

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(cors());
 
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://127.0.0.1:27017')
+MongoClient.connect(process.env.MONGO_SV)
 .then(client => {
     console.log('Connection to database is succesful');
 
