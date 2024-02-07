3a81bc5c7399eaada2ecb2b10b712844e985428f
armin-164 <arminsadr164@hotmail.com>
2024-02-07 22:22:42 +0100

    feat: Install and configurate Dotenv

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 let cors = require('cors');
+require('dotenv').config({ path:'./example.env' });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
