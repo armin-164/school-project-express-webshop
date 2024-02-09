366d9ec0249ac42b60f755b956296bd07d0e52fb
armin-164 <arminsadr164@hotmail.com>
2024-02-09 21:29:37 +0100

    feat: import display cart and add eventlisteners to title and cartIcon for page manipulation

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,13 @@
 import './style.scss';
 import displayHomepage from './scripts/homepage';
 import initializeLoginHandler from './scripts/popupForm';
+import { displayCart } from './scripts/cartManager';
+
+const headerTitle = document.querySelector('h1');
+const cartIcon = document.getElementById('user-cart');
+
+headerTitle.addEventListener('click', displayHomepage);
+cartIcon.addEventListener('click', displayCart);
 
 initializeLoginHandler();
 displayHomepage();
