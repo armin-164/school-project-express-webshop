22f512dd43152f4589a1d028008627c324bafd71
armin-164 <arminsadr164@hotmail.com>
2024-02-09 22:22:38 +0100

    feat: import and add sendOrder as click eventlistener on submit btn
    The eventlistener includes a Promise to first complete displayCart,
    then fetch submitBtn and add the eventlistener.

--- frontend/main.js
+++ frontend/main.js
@@ -2,12 +2,23 @@ import './style.scss';
 import displayHomepage from './scripts/homepage';
 import initializeLoginHandler from './scripts/popupForm';
 import { displayCart } from './scripts/cartManager';
+import { sendOrder } from './scripts/ordersManager';
 
 const headerTitle = document.querySelector('h1');
 const cartIcon = document.getElementById('user-cart');
 
 headerTitle.addEventListener('click', displayHomepage);
-cartIcon.addEventListener('click', displayCart);
+cartIcon.addEventListener('click', () => {
+  // I have added a promise to ensure that submitBtn exists
+  // after completing displayCart before retrieving it
+  new Promise((res, rej) => {
+    displayCart();
+    res();
+  }).then(() => {
+    const submitBtn = document.querySelector('.submit-order');
+    submitBtn.addEventListener('click', sendOrder);
+  });
+});
 
 initializeLoginHandler();
 displayHomepage();
