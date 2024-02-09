86c9ba07cb5e6a50bf430ffd0e48ff86f23e3145
armin-164 <arminsadr164@hotmail.com>
2024-02-09 22:25:19 +0100

    fix: write correct import statement

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ import './style.scss';
 import displayHomepage from './scripts/homepage';
 import initializeLoginHandler from './scripts/popupForm';
 import { displayCart } from './scripts/cartManager';
-import { sendOrder } from './scripts/ordersManager';
+import sendOrder from './scripts/ordersManager';
 
 const headerTitle = document.querySelector('h1');
 const cartIcon = document.getElementById('user-cart');
